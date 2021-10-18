0be5c039ce547b2a28f3f0018670fda14d4033d8
Jascha57 <jakov.petric.student@gmail.com>
2021-10-18 23:53:45 +0200

    Ispravak

--- Zadatak1.c
+++ Zadatak1.c
@@ -13,37 +13,62 @@ typedef struct _Student{
 
 int ProcitajBrojRedakaIzDatoteke(char* nazivDatoteke);
 student* AlocirajMemorijuIProcitajStudente(char* nazivDatoteke, int BrojStudenata);
+
 int main(){
-    int brojRedaka=0,brojac=0;
+    int brojRedaka=0;
+    int brojac = 0;
     char imeDatoteke[]="studenti.txt";
     student* x=NULL;
 
     brojRedaka=ProcitajBrojRedakaIzDatoteke(imeDatoteke);
+
+    if(brojRedaka<=0){
+            printf("Datoteka je prazna!");
+            return 0;
+    }
+
     x=AlocirajMemorijuIProcitajStudente(imeDatoteke, brojRedaka);
+
     if(x==NULL){
         printf("Greska alociranja memorije u main!");
-        return -1;
+        return EXIT_FAILURE;
     }
-    for(brojac=0;brojac<brojRedaka;brojac++)
+
+    for(brojac=0;brojac<brojRedaka;brojac++){
         printf("Ime:%s  \t  Prezime:%s   \t   Abs:%lf   \t   Rel:%lf%%\n", x[brojac].ime, x[brojac].prezime, x[brojac].bodovi, (x[brojac].bodovi)/2);
+    }
+
+    free(x);
 
     return 0;
 }
 
 int ProcitajBrojRedakaIzDatoteke(char* nazivDatoteke){
     int brojac = 0;
+    char DaliJeDatotekaPrazana;
     FILE* datoteka=NULL;
     char buffer[MAX_LINE]={0};
-
+    //Standard otvaranje datoteke
     datoteka=fopen(nazivDatoteke,"r");
+
     if(!datoteka){
         printf("Greska otvaranja datoteke!");
-        return -1;
+        return EXIT_FAILURE;
     }
+    //Provjerimo dali je prazna
+    DaliJeDatotekaPrazana=fgetc(datoteka);
+    if(DaliJeDatotekaPrazana==EOF){
+        return 0;
+    }
+    else{
+        ungetc(DaliJeDatotekaPrazana, datoteka);
+    }
+    //Broji ako nije prazna
     while(!feof(datoteka)){
         fgets(buffer,MAX_LINE,datoteka);
         brojac++;
     }
+    //Zatvaranje datoteke
     fclose(datoteka);
     return brojac;
 }
@@ -60,12 +85,14 @@ student* AlocirajMemorijuIProcitajStudente(char* nazivDatoteke, int BrojStudenat
         free(studenti);
         return NULL;
     }
+
     if(studenti==NULL){
         printf("Greska alociranja memorije!");
         free(studenti);
         fclose(datoteka);
         return NULL;
     }
+
     while(!feof(datoteka)){
         fscanf(datoteka," %s %s %lf", studenti[brojac].ime, studenti[brojac].prezime, &studenti[brojac].bodovi);
         brojac++;
